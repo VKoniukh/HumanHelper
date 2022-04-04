0a5db52776adeab46d9c1ed16e10905483a1f2e4
Volodymyr <konuhvladimir1@gmail.com>
2022-04-04 21:40:28 +0300

    Initial commit

---
+++ src/main/java/ua/helper/humanhelper/HumanHelperApplication.java
@@ -0,0 +1,13 @@
+package ua.helper.humanhelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HumanHelperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HumanHelperApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ua/helper/humanhelper/HumanHelperApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.helper.humanhelper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HumanHelperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
