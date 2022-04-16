0916bb2def8ed30582beca7e3d4e6c5620c9fb6f
Volodymyr <konuhvladimir1@gmail.com>
2022-04-16 23:15:48 +0300

    Google auth was configured

--- src/main/java/ua/helper/humanhelper/service/UserService.java
+++ src/main/java/ua/helper/humanhelper/service/UserService.java
@@ -1,16 +1,11 @@
 package ua.helper.humanhelper.service;
 
-import ua.helper.humanhelper.model.Car;
-import ua.helper.humanhelper.model.CustomOAuth2User;
 import ua.helper.humanhelper.model.User;
-import ua.helper.humanhelper.model.dto.CarDto;
 import ua.helper.humanhelper.model.dto.UserDto;
 
 import java.util.List;
 
 public interface UserService {
-
-    void processOAuthPostLogin(CustomOAuth2User oathUser);
 
     List<User> findAll();
 
